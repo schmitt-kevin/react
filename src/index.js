3db1b9afa49e7c7ddaead24dc0e9afbbce98234d
Kevin Schmitt <kschmitt@novetta.com>
2018-03-30 15:09:10 -0400

    working on searchbar

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//define React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import from seach_bar.js
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
+
+// create a new component. This component should produce some HTML
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
+
+
+
+
+// take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
