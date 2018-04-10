ea69dd8760c472609a3d63384e4067a12f42c9f4
Kevin Schmitt <kschmitt@novetta.com>
2018-04-10 16:37:52 -0400

    got YT api key and getting video response rendered via .map iterator

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 //define React
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import VideoList from "./components/video_list"
 //import from seach_bar.js
 import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
+const API_KEY = 'AIzaSyCDjKrK16HrRk6fjOL5nXi0tpX_WZ4_ZvQ';
+
+
 
 // create a new component. This component should produce some HTML
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] }
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos: videos });
+            // ES6 syntax - this.setState({ videos });
+            // since key and value variable are the same word
+        })
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 
